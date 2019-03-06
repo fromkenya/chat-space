c07b1ff38444907fbedeee79b923a5dc4a64b813
kenya <reo_reo_reo_orz@yahoo.co.jp>
2019-03-06 21:13:19 +0900

    Add two actions & use only two routes in message model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:update, :edit]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages
+    resources :messages, only: [:index, :create]
   end
 end
